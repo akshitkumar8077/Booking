a682dd4c716b276de657b403ecd1b14c89eaa30b
akshitkumar8077 <akshitgupta150@gmail.com>
2022-03-30 12:35:14 +0530

    init

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const makeRequest = async (postObject) => {
+  const url = "https://jsonplaceholder.typicode.com/posts";
+
+  return await fetch(url, {
+    method: "POST",
+    body: JSON.stringify(postObject),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  }).then((response) => response.json());
+};
 const form = document.getElementById("booking-form");
 
 form.addEventListener("submit", (e) => {
@@ -9,5 +20,15 @@ form.addEventListener("submit", (e) => {
   console.log("Thank You, Your Form Is Submitted !!");
   console.log("Patients' Full Name : ", fullName);
   console.log("Patients' Email ID  : ", email);
-});
 
+  const date = new Date(form.elements["date"].value);
+  const data = {
+    fullName: form.elements["fullName"].value,
+    email: form.elements["email"].value,
+    doctor: form.elements["doctor"].value,
+    location: form.elements["location"].value,
+    date: date,
+  };
+
+  makeRequest(data).then((res) => console.log(res));
+});
